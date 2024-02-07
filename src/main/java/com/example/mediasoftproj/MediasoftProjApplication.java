2958edc15ca31b1b16929c3640bb5937ea12b890
Danila Kuzin <danila_kuzin_2001@mail.ru>
2024-02-07 23:02:09 +0400

    Initial commit

---
+++ src/main/java/com/example/mediasoftproj/MediasoftProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediasoftproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediasoftProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediasoftProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mediasoftproj/MediasoftProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediasoftproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediasoftProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
